1c50d52bb187b1c413f6e3d3d88125ab38fd86c2
Julian Frattini <julian.frattini@alice-dsl.net>
2022-12-09 16:30:07 +0100

    Setup initial communication to CiRA API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,51 @@
+import React, { useState } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [sentence, setSentence] = useState("")
+
+  const pipeline_health = (e) => {
+    e.preventDefault()
+
+    fetch('http://localhost:8000/api/health', {
+      method: 'get'
+    }).then(res => res.json())
+      .then(heartbeat => console.log(heartbeat))
+      .catch(function(error) {
+        console.error(error)
+      });
+  }
+
+  const analyze = (e) => {
+    e.preventDefault()
+
+    fetch('http://localhost:8000/api/label', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "sentence": sentence
+      })
+    }).then(res => res.json())
+      .then(labels => {
+        console.log(labels)
+      })
+      .catch(function(error) {
+        console.error(error)
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => analyze(e)}>
+        <input type='text' id='sentence' onChange={e =>setSentence(e.target.value)} value={sentence} ></input>
+        <input type="submit" value='Analyze'></input>
+      </form>
+      <button onClick={(e) => pipeline_health(e)}>Heartbeat</button>
     </div>
   );
 }
