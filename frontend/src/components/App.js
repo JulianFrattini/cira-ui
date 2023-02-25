e62379739dd597b7aa8f4b806bb92ff9c6ee3fb5
Julian Frattini <julian.frattini@alice-dsl.net>
2023-02-25 19:30:02 +0100

    Restructured internal handling of the input field test (closes #9)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './../styles/App.css'
 
 import LabelVisualizer from '../packages/labelvisualization/LabelVisualizer'
@@ -9,64 +9,89 @@ function App() {
 
   const [version, setVersion] = useState("")
 
-  const [sentence, setSentence] = useState("When the red button is pushed or the power fails the system shuts down.")
+  const [fieldSentence, setFieldSentence] = useState("When the red button is pushed or the power fails the system shuts down.")
+  const [analyzing, setAnalyzing] = useState(false)
+
+  const [sentence, setSentence] = useState("")
   const [labels, setLabels] = useState([])
   const [ceg, setCeg] = useState(null)
+
+  useEffect(() => {
+    if (sentence) {
+      fetch('http://localhost:8000/api/label', {
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "sentence": sentence
+        })
+      }).then(res => res.json())
+        .then(jsonlabels => {
+          setLabels(jsonlabels.labels);
+        }).then(() => console.log(labels))
+        .then(() => {
+          return fetch('http://localhost:8000/api/graph', {
+            method: 'PUT',
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              "sentence": sentence,
+              "labels": labels.labels
+            })
+          });
+        })
+        .then(res => res.json())
+        .then(graph => {
+          setCeg(graph.graph)
+        })
+        .then(() => {
+          setAnalyzing(false)
+        })
+        .catch(function (error) {
+          console.error(error)
+        });
+    }
+  }, [sentence]);
 
   // get the heartbeat
   fetch('http://localhost:8000/api/health', {
     method: 'get'
   }).then(res => res.json())
     .then(heartbeat => setVersion(heartbeat["cira-version"]))
-    .catch(function(error) {
+    .catch(function (error) {
       console.error(error)
     });
 
   const analyze = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    fetch('http://localhost:8000/api/label', {
-      method: 'PUT',
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "sentence": sentence
-      })
-    }).then(res => res.json())
-      .then(labels => {
-        setLabels(labels.labels);
-      })
-      .then(() => {
-        return fetch('http://localhost:8000/api/graph', {
-          method: 'PUT',
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            "sentence": sentence,
-            "labels": labels.labels
-          })
-        });
-      })
-      .then(res => res.json())
-      .then(graph => {
-        setCeg(graph.graph)
-      })
-      .catch(function(error) {
-        console.error(error)
-      });
+    setAnalyzing(true)
+    setLabels([])
+    setCeg({})
+    setSentence(fieldSentence)
   }
 
   return (
     <div className="App">
       <p>{version}</p>
       <form onSubmit={(e) => analyze(e)}>
-        <input type='text' id='sentence' onChange={e => setSentence(e.target.value)} value={sentence} ></input>
+        <input type='text' id='sentence' onChange={e => setFieldSentence(e.target.value)} value={fieldSentence} autoFocus></input>
         <input type="submit" value='Analyze'></input>
       </form>
-      {labels.length > 0 && <LabelVisualizer text={sentence} labels={labels}></LabelVisualizer>}
-      {ceg != null && <CEGVisualizer ceg={ceg}></CEGVisualizer>}
+      {!analyzing && labels.length > 0 &&
+        <div>
+          <h2>Labels</h2>
+          <LabelVisualizer text={sentence} labels={labels}></LabelVisualizer>
+        </div>
+      }
+      {!analyzing && ceg != null && 
+        <div>
+          <h2>Cause-Effect-Graph</h2>
+          <CEGVisualizer ceg={ceg}></CEGVisualizer>
+        </div>
+      }
     </div>
   );
 }
